300aed560b433966f78eb2869171090ac8917e03
anirudhsom123 <anirudhsom007@gmail.com>
2025-11-18 09:00:22 +0530

    project initialized

---
+++ src/main/java/com/minorproject/mailingAssistant/MailingAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.minorproject.mailingAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailingAssistantApplication.class, args);
+	}
+
+}
